3ce6ca3ef1a1d2111c956335150447191a8a572c
VittoriaBasile <vichy.basile@gmail.com>
2023-04-04 16:58:04 +0200

    commit

--- src/components/MyNav.jsx
+++ src/components/MyNav.jsx
@@ -1,9 +1,13 @@
 import { Navbar, Nav, Container } from "react-bootstrap";
+import logo from "../logo/logo1.jpg";
 
 const MyNav = () => (
-  <Navbar bg="dark" variant="dark">
+  <Navbar bg="light" variant="light">
     <Container fluid>
-      <Navbar.Brand href="#home">EpiBooks</Navbar.Brand>
+      <Navbar.Brand href="#home">
+        <img className="logo me-2" src={logo} alt="logo" />
+        EpiBooks
+      </Navbar.Brand>
       <Nav className="me-auto">
         <Nav.Link href="#home">Shop</Nav.Link>
         <Nav.Link href="#about">About</Nav.Link>
