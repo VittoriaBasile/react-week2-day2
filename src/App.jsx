4864f477b89e04c3560248706255ed9a2c550d33
VittoriaBasile <vichy.basile@gmail.com>
2023-04-04 15:33:42 +0200

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,22 @@
-import 'bootstrap/dist/css/bootstrap.min.css'
-import './App.css'
-import MyNav from './components/MyNav'
-import MyFooter from './components/MyFooter'
-import MyJumbotron from './components/MyJumbotron'
-// import AllTheBooks from './components/AllTheBooks'
-import { Container } from 'react-bootstrap'
-import BookList from './components/BookList'
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import MyNav from "./components/MyNav";
+import MyFooter from "./components/MyFooter";
+import MyJumbotron from "./components/MyJumbotron";
+import { Container } from "react-bootstrap";
+import BookList from "./components/BookList";
 
-import fantasy from './data/fantasy.json'
+import fantasy from "./data/fantasy.json";
 
 function App() {
   return (
     <Container>
       <MyNav />
       <MyJumbotron />
-      {/* <AllTheBooks /> */}
       <BookList books={fantasy} />
       <MyFooter />
     </Container>
-  )
+  );
 }
 
-export default App
+export default App;
